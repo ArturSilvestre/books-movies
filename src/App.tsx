ed87bab6aafd3c4ce4e3a836d6017c6c33297408
Artur Oliveira <artur.oliveira@avenuecode.com>
2023-03-02 16:10:10 -0300

    :lipstick: feat: Criado CSS Global e ThemeDefault

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-function App() {
+import { ThemeProvider } from "styled-components"
+import { GlobalStyle } from "./styles/global"
+import { defaultTheme } from "./styles/themes/default"
+
+export function App() {
 
   return (
-    <h1>Books & Movies <small> | New York times</small></h1>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+
+      <h1>Books & Movies <small> | New York times</small></h1>
+    </ThemeProvider>
   )
 }
-
-export default App
