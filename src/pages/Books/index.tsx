5822553795fccd24abafa76845bca1ac45a83724
Artur Oliveira <artur.oliveira@avenuecode.com>
2023-03-17 18:34:52 -0300

    :lipstick: feat: Add Estilo Pagina Books

--- src/pages/Books/index.tsx
+++ src/pages/Books/index.tsx
@@ -1,10 +1,22 @@
 import { Header } from "../../components/Header/header";
+import { BooksContent } from "./styles";
 
 export function Books() {
   return (
     <>
       <Header />
-      <h1>Page Books</h1>
+      <BooksContent>
+        <h1>Pagina com listagens dos Livros</h1>
+        <ul>
+          <li>Livro 1: 1</li>
+          <li>Livro 2: 2</li>
+          <li>Livro 3: 3</li>
+          <li>Livro 4: 4</li>
+          <li>Livro 5: 5</li>
+          <li>Livro 6: 6</li>
+          <li>Livro 7: 7</li>
+        </ul>
+      </BooksContent>
     </>
   )
 }
