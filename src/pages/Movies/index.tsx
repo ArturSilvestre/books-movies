d67afd94572bc4fc20702d411e74a4056be0361a
Artur Oliveira <artur.oliveira@avenuecode.com>
2023-03-17 18:32:43 -0300

    :sparkles: feat: Add Pagina Movies

--- src/pages/Movies/index.tsx
+++ src/pages/Movies/index.tsx
@@ -1,10 +1,22 @@
 import { Header } from "../../components/Header/header";
+import { MoviesContent } from "./styles";
 
 export function Movies() {
   return (
     <>
       <Header />
-      <h1>Page Movies</h1>
+      <MoviesContent>
+        <h1>Pagina com listagens dos filmes</h1>
+        <ul>
+          <li>Filme 1: 1</li>
+          <li>Filme 2: 2</li>
+          <li>Filme 3: 3</li>
+          <li>Filme 4: 4</li>
+          <li>Filme 5: 5</li>
+          <li>Filme 6: 6</li>
+          <li>Filme 7: 7</li>
+        </ul>
+      </MoviesContent>
     </>
   )
-}
+} 
