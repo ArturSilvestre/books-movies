20ce708830761a7b2c2eca4564bbe7db36b23b72
Artur Oliveira <artur.oliveira@avenuecode.com>
2023-03-17 18:35:11 -0300

    :lipstick: feat: Add Estilo Pagina Moviess

--- src/pages/Movies/styles.ts
+++ src/pages/Movies/styles.ts
@@ -2,7 +2,27 @@ import styled from "styled-components";
 
 export const HeaderHomeContainer = styled.div`
   background-color: ${props => props.theme['gray-900']};
+  padding: 1.5rem 0 1.5rem;
 `;
 
-export const MainMoviesContainer = styled.main`
-`;
+export const MoviesContent = styled.main`
+  width: 100%;
+  max-width: 1120px;
+  margin: 0 auto;
+  padding: 0 1.5rem;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  
+  h1 {
+    margin: 10px 0;
+  }
+  
+  ul li {
+    text-decoration: none;
+    list-style: none;
+    margin: 15px 0;
+  }
+`; 
